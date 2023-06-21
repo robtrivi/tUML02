ebeaea03b3da6f04df1c4b314cd322cbe2bbaddf
paulethTandazo <ptandazo@espol.edu.ec>
2023-06-21 15:50:15 -0500

    clase ActividadSumativa,Curso,Estudiante,Evaluacion,Profesor,Tarea

---
+++ tCodeUML/src/main/java/com/mycompany/tcodeuml/ActividadSumativa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tcodeuml;
+
+
+import java.util.Date;
+
+/**
+ *
+ * @author marle
+ */
+public abstract class ActividadSumativa {
+    protected String titulo;
+    protected Date fechaDeEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
+    protected String id;
+    
+  
+}
---
+++ tCodeUML/src/main/java/com/mycompany/tcodeuml/Curso.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tcodeuml;
+
+/**
+ *
+ * @author marle
+ */
+public class Curso {
+    private  String id;
+    private String nombre;
+    private boolean estadoDisponible;
+    
+    
+}
